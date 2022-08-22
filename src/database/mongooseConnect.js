f8ef5d0b7aa4dba03457c92ba65e4577342ad6b7
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-08-22 16:10:05 -0300

    configurando/conectando banco de dados

---
+++ src/database/mongooseConnect.js
@@ -0,0 +1,21 @@
+require('dotenv-safe').config()
+const mongoose = require("mongoose")
+
+const DATABASE_URI = process.env.DATABASE_URI
+
+const connect = async () => {
+    try {
+        await mongoose.connect(DATABASE_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        console.log("Banco de dados conectado com sucesso!")
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+module.exports = {
+    connect
+}
